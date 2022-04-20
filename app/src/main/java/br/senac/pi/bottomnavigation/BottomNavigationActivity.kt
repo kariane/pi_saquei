9c9591865adb14da93ee65ce9a15b0cd463fcf97
Fábio Abenza 2 <prof.fabio.abenza@gmail.com>
2022-04-20 18:43:36 -0300

    Commit inicial

---
+++ app/src/main/java/br/senac/pi/bottomnavigation/BottomNavigationActivity.kt
@@ -0,0 +1,40 @@
+package br.senac.pi.bottomnavigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.senac.pi.R
+import br.senac.pi.databinding.ActivityBottomNavigationBinding
+import br.senac.pi.fragments.AlbumsFragment
+import br.senac.pi.fragments.ArtistsFragment
+import br.senac.pi.fragments.RecentsFragment
+
+class BottomNavigationActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityBottomNavigationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.artistas -> {
+                    val frag = ArtistsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+                R.id.albuns -> {
+                    val frag = AlbumsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+                else -> {
+                    val frag = RecentsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+            }
+
+            true
+        }
+    }
+
+}
