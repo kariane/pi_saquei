385f17226fbd38713896618fabb2b7dd009600a5
wellington.lmelo1 <wellingtonlira826@gmail.com>
2022-04-27 20:43:34 -0300

    aula_20220427

--- app/src/main/java/com/example/pi/ui/LoginActivity.kt
+++ app/src/main/java/com/example/pi/ui/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.pi.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pi.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
